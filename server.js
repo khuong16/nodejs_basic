3d4851f3234258cfde132b82e86e855d0c95ecf2
khuong16 <khuong16>
2022-01-07 17:50:41 +0700

    init project 2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
     console.log('run request ...')
     res.setHeader('Content-Type', 'text/html');
     res.write('<h3>Hello world! </h3>');
-    res.write('<h2>from ERIC & HOI DAN IT </h2>');
+    res.write('<h2> </h2>');
     res.end();
 })
 
