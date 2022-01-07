3d56f0b0b3111b142c98291b0da049a23d46a543
khuong16 <khuong16>
2022-01-07 20:32:54 +0700

    video5_helloworld_express

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-const http = require('http');
 
-const server = http.createServer((req, res) => {
-    console.log('run request ...')
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h3>Hello world! </h3>');
-    res.write('<h2> </h2>');
-    res.end();
+const express = require('express')
+const app = express()
+const port = 3000
+
+// router
+app.get('/', (req, res) => {
+    res.send('Hello World!')
 })
 
-server.listen(3000, 'localhost', () => {
-    console.log('Node.JS server is running on port: 3000');
+app.get('/about', (req, res) => {
+    res.send('Minh Khương')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
 })
