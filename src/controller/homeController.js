1ba7ac651b8c9d51e6bd5ee34ddd5aab76ce8c6f
khuong16 <khuong16>
2022-01-08 19:55:31 +0700

    upload file_video16

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,4 +1,6 @@
 import pool from '../configs/connectDB';
+import multer from 'multer';
+
 
 // get data
 let getHomePage = async (req, res) => {
@@ -53,6 +55,39 @@ let postUpdateUser = async (req, res) => {
     return res.redirect('/');
 }
 
+
+let getUploadFilePage = async (req, res) => {
+    return res.render('uploadFile.ejs');
+}
+
+// xử lý với name 'profile_pic
+const upload = multer().single('profile_pic');
+
+let handleUploadFile = async (req, res) => {
+    // 'profile_pic' is the name of our file input field in the HTML form
+
+    upload(req, res, function (err) {
+        // req.file contains information of uploaded file
+        // req.body contains information of text fields, if there were any
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.file) {
+            return res.send('Please select an image to upload');
+        }
+        else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        }
+        else if (err) {
+            return res.send(err);
+        }
+
+        // Display uploaded image for user validation
+        res.send(`You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`);
+    });
+}
+
 module.exports = {
-    getHomePage, getDetailPage, createNewUser, deleteUser, getEditPage, postUpdateUser
+    getHomePage, getDetailPage, createNewUser, deleteUser, getEditPage, postUpdateUser, getUploadFilePage, handleUploadFile
 }
--- src/route/web.js
+++ src/route/web.js
@@ -2,9 +2,34 @@
 // sử dụng method là router() để định nghĩa các router tương ứng.
 
 import express from "express";
-import { home } from "nodemon/lib/utils";
 import homeController from '../controller/homeController';
 // Định nghĩa ra một cái biến router để sử dụng với Router.
+import multer from 'multer';
+import path from 'path';
+var appRoot = require('app-root-path');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, appRoot + "/src/public/image/");
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const imageFilter = function (req, file, cb) {
+    // Accept images only
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+        req.fileValidationError = 'Only image files are allowed!';
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+
+let upload = multer({ storage: storage, fileFilter: imageFilter });
+
 let router = express.Router();
 
 // Định nghĩa các router:
@@ -27,6 +52,11 @@ const initWebRoute = (app) => {
 
     // update user
     router.post('/update-user', homeController.postUpdateUser)
+
+    router.get('/upload', homeController.getUploadFilePage);
+
+    // sau khi router cần có 1 middleware là upload.single
+    router.post('/upload-profile-pic', upload.single('profile_pic'), homeController.handleUploadFile);
 
     // Return về đường link mặc định để mình init tất cả router.
     // Gọi chung là tiền tố thêm vào.
